42d8a033224937d86ad0123ffac5cc9c86c9c749
July541 <zhulei1995@126.com>
2019-01-19 01:00:18 +0800

    Two way

---
+++ Haskell/MyList.hs
@@ -0,0 +1,9 @@
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 :-:
+data MyList a = Empty | a :-: (MyList a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: MyList a -> MyList a -> MyList a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
